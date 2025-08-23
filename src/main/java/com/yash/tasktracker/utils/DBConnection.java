4a89f87b3836d12ee1185809759f7af6a3092ee0
S dudhani <sdudhani9@gmail.com>
2025-08-23 12:54:11 +0530

    user implementation v1

--- src/main/java/com/yash/tasktracker/utils/DBConnection.java
+++ src/main/java/com/yash/tasktracker/utils/DBConnection.java
@@ -6,12 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/tasktracker";
-    private static final String USER = "root";   // your MySQL username
-    private static final String PASSWORD = "root"; // your MySQL password
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // ✅ load driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new SQLException("MySQL Driver not found!", e);
         }
